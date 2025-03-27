85afb3f209b68d3c85f794802e338e99041f5696
FxmousKid <iyan005@outlook.com>
2025-03-27 19:56:28 +0100

    wip(CPP07): work in progress ex01

---
+++ CPP07/ex01/includes/iter.hpp
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   iter.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: inazaria <inazaria@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/27 19:38:17 by inazaria          #+#    #+#             */
+/*   Updated: 2025/03/27 19:48:14 by inazaria         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+#include <cstddef>
+#include <iostream>
+
+template <typename T> void iter(T *arr, size_t len, void f(T))
+{
+	size_t i;
+
+	i = 0;
+	while (i < len)
+	{
+		f(arr[i]);
+		i++;
+	}
+}
+
+template <typename T> void print_item(T item)
+{
+	std::cout << item << " ";
+}
---
+++ CPP07/ex01/src/main.cpp
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: inazaria <inazaria@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/27 19:35:16 by inazaria          #+#    #+#             */
+/*   Updated: 2025/03/27 19:52:08 by inazaria         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <cstdlib>
+#include <iostream>
+
+#include "iter.hpp"
+
+
+int main(void)
+{
+	int arr[] = {1, 2, 3, 4, 5};
+	iter(arr, 5, print_item);
+	std::cout << std::endl;
+
+	int arr2[] = {};
+	iter(arr2, 2, print_item);
+	std::cout << std::endl;
+
+	iter(arr, 5, print_item);
+	std::cout << std::endl;
+
+	iter(arr, 5, print_item);
+	std::cout << std::endl;
+	
+	std::cout << std::endl;
+	
+	return (EXIT_SUCCESS);
+}
